61b9ffc08f91ed548a6689612df1409158da57a6
Philipp Riegelmann <p.riegelmann@queo.onmicrosoft.com>
2024-05-17 10:19:12 +0200

    Adding TransactionsManger

---
+++ src/Commons.Persistence.EntityFramework/Infrastructure/TransactionManager/GenericTransactionManager.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore;
+using Queo.Commons.Persistence.Infrastructure.TransactionManager;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Queo.Commons.Persistence.EntityFramework.Infrastructure.TransactionManager
+{
+    /// <summary>
+    /// TransactionManager class.
+    /// </summary>
+    public class GenericTransactionManager<T> : ITransactionManager where T : DbContext
+    {
+        private readonly T _dbContext;
+        private IDbContextTransaction? _transaction;
+        private readonly ILogger<GenericTransactionManager<T>> _logger;
+
+        /// <summary>
+        /// ctor.
+        /// </summary>
+        /// <param name="dbContext"></param>
+        public GenericTransactionManager(T dbContext, ILogger<GenericTransactionManager<T>> logger)
+        {
+            _dbContext = dbContext;
+            _logger = logger;
+        }
+
+        /// <inheritdoc />
+        public void BeginTransaction()
+        {
+            _transaction = _dbContext.Database.BeginTransaction();
+        }
+
+        /// <inheritdoc />
+        public async Task BeginTransactionAsync()
+        {
+            _transaction = await _dbContext.Database.BeginTransactionAsync();
+        }
+
+        /// <inheritdoc />
+        public void CommitTransaction()
+        {
+            if (TransactionIsActive())
+            {
+                _transaction!.Commit();
+            }
+            else
+            {
+                _logger.LogWarning("No transaction to commit.");
+            }
+        }
+
+        /// <inheritdoc />
+        public async Task CommitTransactionAsync()
+        {
+            if (TransactionIsActive())
+            {
+                await _transaction!.CommitAsync();
+            }
+            else
+            {
+                _logger.LogWarning("No transaction to commit.");
+            }
+        }
+
+        /// <inheritdoc />
+        public void RollbackTransaction()
+        {
+            if (TransactionIsActive())
+            {
+                _transaction!.Rollback();
+                DeleteTransaction();
+            }
+            else
+            {
+                _logger.LogWarning("No transaction to rollback.");
+            }
+        }
+
+        /// <inheritdoc />
+        public async Task RollbackTransactionAsync()
+        {
+            if (TransactionIsActive())
+            {
+                await _transaction!.RollbackAsync();
+                DeleteTransaction();
+            }
+            else
+            {
+                _logger.LogWarning("No transaction to rollback.");
+            }
+        }
+
+        /// <inheritdoc />
+        public bool TransactionIsActive()
+        {
+            return _transaction != null;
+        }
+
+        private void DeleteTransaction()
+        {
+            _transaction = null;
+        }
+    }
+}
---
+++ src/Commons.Persistence/Infrastructure/TransactionManager/ITransactionManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+
+namespace Queo.Commons.Persistence.Infrastructure.TransactionManager
+{
+    /// <summary>
+    /// TransactionManager interface.
+    /// </summary>
+    public interface ITransactionManager
+    {
+        /// <summary>
+        /// Begins a new transaction.
+        /// </summary>
+        void BeginTransaction();
+
+        /// <summary>
+        /// Begins a new transaction.
+        /// </summary>
+        Task BeginTransactionAsync();
+
+        /// <summary>
+        /// Commits the current transaction.
+        /// </summary>
+        void CommitTransaction();
+
+        /// <summary>
+        /// Commits the current transaction.
+        /// </summary>
+        Task CommitTransactionAsync();
+
+        /// <summary>
+        /// Rolls back the current transaction.
+        /// </summary>
+        void RollbackTransaction();
+
+        /// <summary>
+        /// Rolls back the current transaction.
+        /// </summary>
+        Task RollbackTransactionAsync();
+
+        /// <summary>
+        /// Returns whether a transaction is active.
+        /// </summary>
+        bool TransactionIsActive();
+    }
+}
