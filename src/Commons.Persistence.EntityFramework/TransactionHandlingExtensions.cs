99845349a41e4dce1a3ea5da470ad79a4bf04b3b
Philipp Riegelmann <p.riegelmann@queo.onmicrosoft.com>
2024-06-14 15:40:03 +0200

    Create TransactionHandlingExtensions

---
+++ src/Commons.Persistence.EntityFramework/TransactionHandlingExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Queo.Commons.Persistence.EntityFramework.TransactionManager;
+using Queo.Commons.Persistence.Filter;
+using Queo.Commons.Persistence.TransactionManager;
+
+
+namespace Queo.Commons.Persistence.EntityFramework
+{
+    public static class TransactionHandlingExtensions
+    {
+        public static void UseTransactionHandling<TDbContext>(this IServiceCollection services) where TDbContext : DbContext
+        {
+            services.AddScoped<ITransactionManager, GenericTransactionManager<TDbContext>>();
+
+            // Add TransactionCommitFilter
+            services.AddMvc(options =>
+            {
+                options.Filters.Add<TransactionCommitFilter>();
+            });
+
+            // Add TransactionFilter
+            services.AddMvc(options =>
+            {
+                options.Filters.Add<TransactionFilter>();
+            });
+
+            // Add TransactionRollbackOnExceptionFilter
+            services.AddMvc(options =>
+            {
+                options.Filters.Add<TransactionRollbackOnExceptionFilter>();
+            });
+        }
+    }
+}
