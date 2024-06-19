9277a68ed9cd10d07135f2464527caad68526b52
Philipp Riegelmann <p.riegelmann@queo.onmicrosoft.com>
2024-06-19 12:30:12 +0200

    Add Summary to method and updated Readme

--- src/Commons.Persistence.EntityFramework/TransactionHandlingExtensions.cs
+++ src/Commons.Persistence.EntityFramework/TransactionHandlingExtensions.cs
@@ -4,11 +4,18 @@ using Queo.Commons.Persistence.EntityFramework.TransactionManager;
 using Queo.Commons.Persistence.Filter;
 using Queo.Commons.Persistence.TransactionManager;
 
-
 namespace Queo.Commons.Persistence.EntityFramework
 {
+    /// <summary>
+    /// Transaction handling extensions for the service collection.
+    /// </summary>
     public static class TransactionHandlingExtensions
     {
+        /// <summary>
+        /// Adds the transaction handling to the service collection.
+        /// </summary>
+        /// <typeparam name="TDbContext"></typeparam>
+        /// <param name="services"></param>
         public static void UseTransactionHandling<TDbContext>(this IServiceCollection services) where TDbContext : DbContext
         {
             services.AddScoped<ITransactionManager, GenericTransactionManager<TDbContext>>();
